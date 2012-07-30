069a505d24d235e6d2aa117b7f0e26c1ba08178a
ap4y <lod@pisem.net>
2012-07-30 18:24:30 +0400

    changed response format (removed 404 to handle jsonp errors on client)

--- app.js
+++ app.js
@@ -2,39 +2,41 @@ var app = require('express').createServer(),
     Lastfm = require('./lastfm'),
     client = new Lastfm();
 
-app.get('/genre_topalbums/:genre/', function(req, res) {
+client.setShouldCache(true);
+
+app.get('/genre_topalbums/:genre', function(req, res) {
   client.tag_getTopAlbums(req.params.genre, function(data) {
-    res.send(data ? data : 404);
+    res.json(data ? data : {});
   });
 });
 
-app.get('/genre_image/:genre/', function(req, res) {
-  client.tag_getImages(req.params.genre, function(data) {
-    res.send(data ? data : 404);
+app.get('/genre_image/:genre', function(req, res) {
+  client.tag_getImages(req.params.genre, function(images) {
+    res.json(images ? images : {});
   });
 });
 
-app.get('/artist_topalbums/:artist/', function(req, res) {
+app.get('/artist_topalbums/:artist', function(req, res) {
   client.artist_getTopAlbums(req.params.artist, function(data) {
-    res.send(data ? data : 404);
+    res.json(data ? data : {});
   });
 });
 
-app.get('/artist_image/:artist/', function(req, res) {
+app.get('/artist_image/:artist', function(req, res) {
   client.artist_getImages(req.params.artist, function(images) {
-    res.send(images ? images : 404);
+    res.json(images ? images : {});
   });
 });
 
 app.get('/album_info/:artist/:album', function(req, res) {
   client.album_getInfo(req.params.album, req.params.artist, function(data) {
-    res.send(data ? data : 404);
+    res.json(data ? data : {});
   });
 });
 
 app.get('/album_image/:artist/:album', function(req, res) {
   client.album_getImages(req.params.album, req.params.artist, function(images) {
-    res.send(images ? images : 404);
+    res.json(images ? images : {});
   });
 });
 
